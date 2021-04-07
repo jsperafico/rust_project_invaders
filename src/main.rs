804a1898acaee4e9744df5766a9e271a2d82a01d
jsperafico <jhonny.potter@gmail.com>
2021-04-07 15:45:36 +0000

    sound configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rusty_audio::Audio;
+
+fn main() -> Result <(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode","resource/sound/explode.wav");
+    audio.add("lose","resource/sound/lose.wav");
+    audio.add("move","resource/sound/move.wav");
+    audio.add("pew","resource/sound/pew.wav");
+    audio.add("startup","resource/sound/startup.wav");
+    audio.add("win","resource/sound/win.wav");
+    audio.play("startup");
+
+    audio.wait();
+    Ok(())
 }
